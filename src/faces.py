de86bf1f5542f2a99191abac3e8d1326ec83b623
Abhishek N <abhisheknshetty97@gmail.com>
2020-02-11 14:02:32 +0530

    Add multiple changes

--- src/faces.py
+++ src/faces.py
@@ -1,13 +1,39 @@
 import numpy as numpy
 import cv2
 
+face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+recognizer=cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("./recognizers/face-trainner.yml")
+
+labels = {"person_name": 1}
+with open("labels.pickle", 'rb') as f:
+    og_labels = pickle.load(f)                  #original
+    labels = {v:k for k, v in labels.items()}   #key value pair
+
 cap = cv2.VideoCapture(0)
 
 while(True):
     #capture each frames
     ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.5, minNeighbors = 5)
+    for(x, y, w, h) in faces:
+        print(x, y, w, h)
+        roi_gray = gray[y:y+h, x:x+w] #(ycord_start, ycord_end)    #display resulting frame
+        roi_color = frame[y:y+h, x:x+w]
 
-    #display resulting frame
+        id_, conf = recognizer.predict(roi_gray)
+        if conf >= 45 and conf <= 85:
+            print(id_)
+            print(labels[id_])
+
+        img_item = "seb1.jpg"
+        cv2.imwrite(img_item, roi_color)
+        color = (255, 0 , 0) #BGR 0-255
+        stroke = 2
+        end_cord_x = x + w
+        end_cord_y = y + h
+        cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
     cv2.imshow('frame', frame)
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
